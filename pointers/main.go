178208c6777d05924ee5ae635893239102619b65
sabirz <sabir@247waiter.com>
2015-01-27 21:23:10 -0500

    pointer with 2 variables
    updated to include pointer with 2 variables.

--- pointers/main.go
+++ pointers/main.go
@@ -5,12 +5,18 @@ import (
 )
 
 func main() {
+	x, y := 20, 40
+
 	num := 5
 	double(num)
 	fmt.Println(num)
 
 	triple(&num)
 	fmt.Println(num)
+
+	swap(&x, &y)
+	fmt.Println(x, y)
+
 }
 
 func double(n int) {
@@ -19,4 +25,9 @@ func double(n int) {
 
 func triple(n *int) {
 	*n = *n * 3
+}
+
+func swap(x, y *int) {
+	*x = *x * 5
+	*y = *y * 5
 }
