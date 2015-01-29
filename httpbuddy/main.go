e5db5c20d6054ababa027726563c2dac103d61ac
sabirz <sabir@247waiter.com>
2015-01-28 19:50:21 -0500

    Playing with Flags
    Playing with Flags

--- httpbuddy/main.go
+++ httpbuddy/main.go
@@ -9,11 +9,16 @@ import (
 
 var port int
 
+//var help string
+
 func main() {
 	flag.IntVar(&port, "port", 3000, "The port to run the server on")
+
+	help := flag.Int("help", 5, "test test test")
 	flag.Parse()
 
 	fmt.Printf("Serving files on localhost: %v\n", port)
+	fmt.Printf("taking requests on: %v\n", *help)
 
 	err := ServeStatic(port)
 	if err != nil {
