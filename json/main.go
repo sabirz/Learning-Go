a9f56eb6ad733417c18f259434bc7413b2a5e8b8
sabirz <sabir@247waiter.com>
2015-01-30 14:32:26 -0500

    json parser

---
+++ json/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Config map[string]interface{}
+type Config2 map[int]interface{}
+
+func (c Config) Name() string {
+	return c["name"].(string)
+
+}
+
+func (c Config) Author() string {
+	return c["author"].(string)
+}
+
+func LoadConfig(path string) (Config, error) {
+	var m map[string]interface{}
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return m, err
+	}
+
+	err = json.Unmarshal(data, &m)
+	return m, err
+}
+
+func main() {
+
+	config, err := LoadConfig("config.json")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(config)
+	fmt.Println(config.Name())
+	fmt.Println(config.Author())
+
+}
